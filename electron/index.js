2eb1c9fe4a116b30ae9a874a592ff2e65dec2735
haithemkoriche <koriche@tabiblib-services.com>
2024-11-27 02:26:04 +0100

    optimise electron

--- electron/index.js
+++ electron/index.js
@@ -1,53 +1,30 @@
-const { app, BrowserWindow, shell } = require('electron');
-const path = require('path');
-// const AutoLaunch = require('electron-auto-launch');
+const { app, BrowserWindow } = require('electron');
 const { exec } = require('child_process');
+const path = require('path');
+
 let mainWindow;
-// const Registry = require('electron-windows-registry');
 
-// Configuration pour démarrer automatiquement l'application
-// const myAppLauncher = new AutoLaunch({
-//     name: 'Dr.Benhamada', // Remplacez par le nom de votre application
-//     path: process.execPath,         // Chemin vers le fichier exécutable de votre application
-// });
+function startLaravelServer() {
+    exec('php artisan serve --host=0.0.0.0 --port=8000', { cwd: path.join('C:\\xampp\\htdocs\\Dr.Benhamada') }, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Erreur Artisan Serve : ${error.message}`);
+            return;
+        }
+        console.log(`Serveur Laravel démarré : ${stdout}`);
+    });
+}
 
-// myAppLauncher.isEnabled()
-//     .then((isEnabled) => {
-//         if (!isEnabled) {
-//             return myAppLauncher.enable(); // Active le démarrage automatique si ce n'est pas encore fait
-//         }
-//     })
-//     .catch((err) => {
-//         console.error('Erreur lors de l\'activation de l\'auto-lancement :', err);
-//     });
-// function createWindow() {
-//     const win = new BrowserWindow({
-//         width: 1280,
-//         height: 720,
-//         webPreferences: {
-//             nodeIntegration: true,
-//             contextIsolation: false,
-//             enableRemoteModule: true,
-//         },
-//     });
+function startMySQLServer() {
+    exec('C:\\xampp\\mysql_start.bat', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Erreur MySQL : ${error.message}`);
+            return;
+        }
+        console.log(`MySQL démarré : ${stdout}`);
+    });
+}
 
-//     // Charger l'application Vuexy en mode production
-//     win.loadURL('http://localhost:8000'); // L'URL de votre application Laravel en local
-// }
-
-app.on('ready', () => {
-    // const autoLaunchKey = new Registry({
-    //     hive: 'HKCU',
-    //     key: '\\Software\\Microsoft\\Windows\\CurrentVersion\\Run',
-    // });
-    // autoLaunchKey.set('Dr.Benhamada', process.execPath, Registry.REG_SZ, (err) => {
-    //     if (err) {
-    //         console.error('Erreur lors de l\'ajout à l\'auto-lancement :', err);
-    //     } else {
-    //         console.log('L\'application est configurée pour démarrer au lancement de Windows.');
-    //     }
-    // });
-    // Créer la fenêtre Electron
+function createMainWindow() {
     mainWindow = new BrowserWindow({
         width: 1280,
         height: 800,
@@ -56,35 +33,16 @@ app.on('ready', () => {
         },
     });
 
-    // Lancer `php artisan serve`
-    exec('php artisan serve --host=127.0.0.1 --port=8000', { cwd: 'C:\\xampp\\htdocs\\Dr.Benhamada' }, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Erreur Artisan Serve : ${error.message}`);
-            return;
-        }
-        console.log(`Serveur Laravel démarré : ${stdout}`);
-    });
-    exec('npm run dev', { cwd: 'C:\\xampp\\htdocs\\Dr.Benhamada' }, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Erreur NPM Run Dev : ${error.message}`);
-            return;
-        }
-        console.log(`Vue.js démarré : ${stdout}`);
-    });
-    exec('C:\\xampp\\mysql_start.bat', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Erreur MySQL : ${error.message}`);
-            return;
-        }
-        console.log(`MySQL démarré : ${stdout}`);
-    });
-
-    // Attendre que les serveurs soient prêts avant de charger l'URL
     setTimeout(() => {
         mainWindow.loadURL('http://127.0.0.1:8000');
-    }, 5000); // Délai de 5 secondes
-});
+    }, 1000);
+}
 
+app.on('ready', () => {
+    startLaravelServer();
+    startMySQLServer();
+    createMainWindow();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -94,6 +52,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
+        createMainWindow();
     }
 });
